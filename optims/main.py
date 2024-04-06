5a31360662463fdde3969b5c7c7947ba0ada0d6d
Qw11111111111 <louis.meller@icloud.com>
2024-04-06 13:43:13 +0200

    implemented SGD

--- optims/main.py
+++ optims/main.py
@@ -1,13 +1,20 @@
+from colorsys import yiq_to_rgb
 import numpy as np
 from supers.main import *
 
 class SGD(optim):
 
-    def __init__(self) -> None:
+    def __init__(self, batch_size: int = 1, momentum: bool = False, alpha: float = 0.01) -> None:
         super().__init__()
+        self.batch_size = batch_size
     
-    def backpropagation(self) -> None:
-        return super().backpropagation()
+    def backpropagation(self, X: np.ndarray | float, Y: np.ndarray | float) -> None:
+        random_perm = np.random.permutation(len(Y))
+        X, Y = X[random_perm], Y[random_perm]
+        for i in range(int(len(X) / self.batch_size) - 1):
+            X_train, Y_train = X[i:i + self.batch_size], Y[i:i + self.batch_size]
+            super().backpropagation(X_train, Y_train)
+        
     
 class Adam(optim):
 
