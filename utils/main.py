9c5faa3e9b3267bf5c9ccdc22fb64c0a474202ed
Qw11111111111 <louis.meller@icloud.com>
2024-04-09 15:57:00 +0200

    added CV class

--- utils/main.py
+++ utils/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sympy import per
 
 def MSELoss(X: np.ndarray | int = None, Y: np.ndarray | int = None, w: np.ndarray | int = None, pred: np.ndarray | int = None, mode: str = "forward") -> float | np.ndarray:
     if X and w:
@@ -20,3 +21,20 @@ def argwhere(v: list | np.ndarray, val: str | float) -> int:
     for i, value in enumerate(v):
         if str(value) == str(val):
             return i
+
+class CV():
+    def __init__(self, cv_folds: int = 5, permute: bool = True):
+        self.cv_folds = cv_folds
+        self.permute = permute
+
+    def get_indices(self, X: np.ndarray):
+        if self.permute:
+            permutation = np.random.permutation(len(X))
+        else:
+            permutation = np.arange(len(X))
+        fold_size = int(len(permutation) / self.cv_folds)
+        indices = [permutation[i * fold_size:i * fold_size + fold_size] for i in range(self.cv_folds)]
+        print(indices)
+        for i in range(self.cv_folds):
+            yield indices[:i] + indices[i + 1:], indices[i]
+    
