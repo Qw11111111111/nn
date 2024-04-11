6370549fffcd5a60c80d594d44715cf0c2fb97e0
Qw11111111111 <louis.meller@icloud.com>
2024-04-11 16:54:56 +0200

    changed MSE loss calculations

--- loss_functions/main.py
+++ loss_functions/main.py
@@ -9,11 +9,11 @@ class MSELoss(Loss):
 
     def get_grad(self, Y: np.ndarray | float, pred: np.ndarray | float, axis: int | bool = 1) -> np.ndarray | float:
         if np.isscalar(Y) and np.isscalar(pred):
-            return 2 * (pred - Y)
+            return (pred - Y)
         
         #assert Y.shape[0] == pred.shape[0]
         #print(Y, pred)
-        return (np.sum(np.dot(2, np.sum([pred, - Y], axis = 0)), axis = int(not axis)) / Y.shape[int(axis)]).reshape(1, Y.shape[1])
+        return 0.5 * (np.sum(np.dot(2, np.sum([pred, - Y], axis = 0)), axis = int(not axis)) / Y.shape[int(axis)]).reshape(1, Y.shape[1])
         return np.sum([2 * (pred[i] - Y[i]) for i in range(len(Y))], axis=1) / len(Y)
     
     def __call__(self, Y: np.ndarray | float, pred: np.ndarray | float, axis: int = 0) -> np.ndarray | float:
