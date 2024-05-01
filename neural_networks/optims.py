0fb65d1a662bdc313397dd97eb2fc91c0f202b9d
Qw11111111111 <louis.meller@icloud.com>
2024-05-01 13:39:13 +0200

    implemented momentum GD

--- neural_networks/optims.py
+++ neural_networks/optims.py
@@ -1,5 +1,5 @@
 import numpy as np
-from parents.parentclasses import *
+from parents.parentclasses import optim, Loss, Module
 
 class SGD(optim):
 
@@ -15,7 +15,7 @@ class SGD(optim):
         for i in range(int(len(X) / self.batch_size) - 1):
             X_train, Y_train = X[i:i + self.batch_size], Y[i:i + self.batch_size]
             super().backpropagation(X_train, Y_train)
-        
+
 
 class Adam(optim):
 
@@ -29,21 +29,44 @@ class Adam(optim):
     
 class Momentum(optim):
     
-    #TODO
+    #TODO: check if momentum is calculated correctly
 
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, lr: float, model: Module, loss: Loss, stop_val: float = 0.0001, beta: float = 5e-1, *args, **kwargs) -> None:
+        super().__init__(lr, model, loss, stop_val, *args, **kwargs)
+        assert beta < 1 and beta > 0, "beta must be in 1 > beta > 0"
+        self.beta = beta
+        self.prev_bias_grads = [0 for _ in range(len(self.model.layers))] if self.model.fit_intercept else None
+        self.prev_weight_grads = [0 for _ in range(len(self.model.layers))]
 
-    def backpropagation(self) -> None:
-        return super().backpropagation()
+    def backpropagation(self, X: np.ndarray, Y: np.ndarray) -> None:
+        super().backpropagation(X, Y)
+        for i, layer in enumerate(self.model.layers):
+            if self.prev_bias_grads is not None:
+                layer.bias_grad =  -self.beta * self.prev_bias_grads[i] + (1 - self.beta) * layer.bias_grad
+                self.prev_bias_grads[i] = layer.bias_grad
+            layer.weight_grad =  -self.beta * self.prev_weight_grads[i] + (1 - self.beta) * layer.weight_grad
+            self.prev_weight_grads[i] = layer.weight_grad
+            layer.update_state_dict(-self.lr * layer.weight_grad, -self.lr * layer.bias_grad)
     
 class GD(optim):
 
     def __init__(self, lr: float, model: Module, loss: Loss, stop_val: float = 1e-4) -> None:
+        """initalizes a basic gradient descent optimizer"""
         super().__init__(lr, model, loss, stop_val)
 
     def backpropagation(self, X: np.ndarray, Y: np.ndarray) -> None:
-        return super().backpropagation(X, Y)
+        super().backpropagation(X, Y)
+        for layer in self.model.layers:
+            layer.update_state_dict(-self.lr * layer.weight_grad, -self.lr * layer.bias_grad)
+
 
 class AdaMax(optim):
     pass
+
+class ElasticNet(optim):
+
+    def __init__(self, lr: float, model: Module, loss: Loss, stop_val: float = 0.0001, *args, **kwargs) -> None:
+        super().__init__(lr, model, loss, stop_val, *args, **kwargs)
+
+    def backpropagation(self, X: np.ndarray, Y: np.ndarray) -> None:
+        return super().backpropagation(X, Y)
