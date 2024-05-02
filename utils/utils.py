e5214103133bb0a29b1e7da3235f4c077b352b64
Qw11111111111 <louis.meller@icloud.com>
2024-05-02 17:03:24 +0200

    argwhere foe 2d inputs implemented

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,17 @@
 import numpy as np
+from time import perf_counter
 
-def argwhere(v: list | np.ndarray, val: str | float | int) -> int:
-    for i, value in enumerate(v):
-        if str(value) == str(val):
-            return i
+def argwhere(v: list | np.ndarray, val: str | float | int, axis: int = 0) -> int | tuple[int]:
+    if axis == 0:
+        for i, value in enumerate(v):
+            if str(value) == str(val):
+                return i
+    elif axis == 1:
+        for i, row in enumerate(v):
+            for j, value in enumerate(row):
+                if str(value) == str(val):
+                    return (i, j)
+        
+
+def timeit(func):
+    pass
