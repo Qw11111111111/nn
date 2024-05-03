567dedb3aebde8a73e00a697f10f6adcbf9eee5b
Qw11111111111 <louis.meller@icloud.com>
2024-05-03 17:23:19 +0200

    testing for random choice kmeans

--- testing/test_kmeans.py
+++ testing/test_kmeans.py
@@ -30,14 +30,19 @@ for i in range(centers):
 kmeans = KMeans(centers, n_retries=restarts, verbose=True, init_method="kmeans++")
 assignments, centroids = kmeans.fit_predict(X)
 
+kmeans_random_choice = KMeans(centers, n_retries=restarts, verbose=True, init_method="random_choice")
+assignments_rc, centroids_rc = kmeans_random_choice.fit_predict(X)
+
 kmeans_random = KMeans(centers, n_retries=restarts, verbose=True, init_method="random")
 assignments_r, centroids_r = kmeans_random.fit_predict(X)
 
 nums = [argwhere(assignments, i, axis=1)[0] for i in range(X.shape[0])]
 nums_r = [argwhere(assignments_r, i, axis=1)[0] for i in range(X.shape[0])]
+nums_rc = [argwhere(assignments_r, i, axis=1)[0] for i in range(X.shape[0])]
 
 color_assignments = [nums[i] for i in range(X.shape[0])]
-color_assignments_r = [nums_r[i] for i in range(X.shape[0])]
+color_assignments_r = [nums_r[i] for i in range(X.shape[0])] 
+color_assignments_rc = [nums_rc[i] for i in range(X.shape[0])] 
 
 s_score = kmeans.silhouette(X)
 
@@ -45,11 +50,11 @@ print(s_score, "mine")
 s_score_2 = silhouette_score(X, np.array(color_assignments))
 print(s_score_2, "slearn")
 
-kmean_sk = clus.KMeans(centers)
+kmean_sk = clus.KMeans(centers, init="k-means++")
 labels_sk = kmean_sk.fit_predict(X)
 
 
-fig, ax = plt.subplots(1, 4, figsize=(20, 5))
+fig, ax = plt.subplots(1, 5, figsize=(25, 5))
 ax[0].scatter(centroids[:][:,0], centroids[:][:,1], marker = "P")
 ax[0].scatter(X.T[:][0], X.T[:][1],
             c=[colors[i] for i in color_assignments])
@@ -58,12 +63,16 @@ ax[1].scatter(centroids_r[:][:,0], centroids_r[:][:,1], marker = "P")
 ax[1].scatter(X.T[:][0], X.T[:][1],
             c=[colors[i] for i in color_assignments_r])
 ax[1].set_title("utils.math.kmeans random plot")
+ax[2].scatter(centroids_rc[:][:,0], centroids_rc[:][:,1], marker = "P")
 ax[2].scatter(X.T[:][0], X.T[:][1],
-            c=[colors[i] for i in labels_sk])
-ax[2].set_title("sklearn plot")#ax[1].scatter(X.T[:][0], X.T[:][1],
+            c=[colors[i] for i in color_assignments_rc])
+ax[2].set_title("utils.math.kmeans random choice plot")
 ax[3].scatter(X.T[:][0], X.T[:][1],
+            c=[colors[i] for i in labels_sk])
+ax[3].set_title("sklearn plot")#ax[1].scatter(X.T[:][0], X.T[:][1],
+ax[4].scatter(X.T[:][0], X.T[:][1],
             c=[colors[i] for i in y])
-ax[3].set_title("true blobs")
+ax[4].set_title("true blobs")
 fig.suptitle(f"Plots for Kmeans clustering with my and sklearn implementation")
 plt.show()
 
