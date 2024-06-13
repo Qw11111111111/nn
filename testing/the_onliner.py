99c9a984d4bd040e87cd89b428ec0e1df020bdce
Qw11111111111 <louis.meller@icloud.com>
2024-06-13 18:43:19 +0200

    cleaned up even more

--- testing/the_onliner.py
+++ testing/the_onliner.py
@@ -21,8 +21,5 @@ print(model_2.forward(X))
 plt.plot(model_2.forward(X).detach().numpy(), y.detach().numpy())
 plt.show()
 
-# the concept
-[[[(x := lambda: print("hello"))()], [(x := lambda: print("I"))()], [(x := lambda: print("am"))()], [(x := lambda: print("stupid"))()]] for i in range(1)]
-
 # the above code compressed to one line
-[[[torch.manual_seed(42)], [(train := lambda model, crit, optimizer, X, y: [[[(zero := lambda: optimizer.zero_grad())()], [(get_loss := lambda: crit(model(X), y).backward())()], [(step := lambda: optimizer.step())()]] for _ in range(200)])((model := nn.Sequential(nn.Linear(1,1,False), nn.ReLU(), nn.Linear(1,1,False))), nn.MSELoss(), torch.optim.SGD(model.parameters(), 6e-7), X := torch.tensor([100.0, 200.0, 300.0, 400.0, 500.0]).unsqueeze(0).T, y := torch.tensor([10.0, 20.0, 30.0, 40.0, 50.0]).unsqueeze(0).T)], [(printer := lambda: print(model.forward(X)))()], [(plotter := lambda: plt.plot(model.forward(X).detach().numpy(), y.detach().numpy()))()], [(show := lambda: plt.show())()]] for _ in range(1)]
+[[torch.manual_seed(42), (train := lambda model, crit, optimizer, X, y: [[optimizer.zero_grad(), crit(model(X), y).backward(), optimizer.step()] for _ in range(200)])((model := nn.Sequential(nn.Linear(1,1,False), nn.ReLU(), nn.Linear(1,1,False))), nn.MSELoss(), torch.optim.SGD(model.parameters(), 6e-7), X := torch.tensor([100.0, 200.0, 300.0, 400.0, 500.0]).unsqueeze(0).T, y := torch.tensor([10.0, 20.0, 30.0, 40.0, 50.0]).unsqueeze(0).T), print(model.forward(X)), plt.plot(model.forward(X).detach().numpy(), y.detach().numpy()), plt.show()] for _ in range(1)]
