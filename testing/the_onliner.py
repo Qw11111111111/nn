f0f8df551f6c30127afc69602578533a9985576d
Qw11111111111 <louis.meller@icloud.com>
2024-06-13 18:56:03 +0200

    further improvements

--- testing/the_onliner.py
+++ testing/the_onliner.py
@@ -18,8 +18,13 @@ for _ in range(200):
 
 print(model_2.forward(X))
 
-plt.plot(model_2.forward(X).detach().numpy(), y.detach().numpy())
+plt.plot(X.detach().numpy(), model_2.forward(X).detach().numpy())
+plt.plot(X.detach().numpy(), y.detach().numpy())
+plt.legend(["Shallow net", "truth"])
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.title("Prediction of Shallow Net")
 plt.show()
 
 # the above code compressed to one line
-[[torch.manual_seed(42), (train := lambda model, crit, optimizer, X, y: [[optimizer.zero_grad(), crit(model(X), y).backward(), optimizer.step()] for _ in range(200)])((model := nn.Sequential(nn.Linear(1,1,False), nn.ReLU(), nn.Linear(1,1,False))), nn.MSELoss(), torch.optim.SGD(model.parameters(), 6e-7), X := torch.tensor([100.0, 200.0, 300.0, 400.0, 500.0]).unsqueeze(0).T, y := torch.tensor([10.0, 20.0, 30.0, 40.0, 50.0]).unsqueeze(0).T), print(model.forward(X)), plt.plot(model.forward(X).detach().numpy(), y.detach().numpy()), plt.show()] for _ in range(1)]
+[torch.manual_seed(42), (train := lambda model, crit, optimizer, X, y: [[optimizer.zero_grad(), crit(model(X), y).backward(), optimizer.step()] for _ in range(200)])((model := nn.Sequential(nn.Linear(1,1,False), nn.ReLU(), nn.Linear(1,1,False))), nn.MSELoss(), torch.optim.SGD(model.parameters(), 6e-7), X := torch.tensor([100.0, 200.0, 300.0, 400.0, 500.0]).unsqueeze(0).T, y := torch.tensor([10.0, 20.0, 30.0, 40.0, 50.0]).unsqueeze(0).T), print(model.forward(X)), plt.plot(X.detach().numpy(), model.forward(X).detach().numpy()), plt.plot(X.detach().numpy(), y.detach().numpy()), plt.ylabel("Y"), plt.xlabel("X"), plt.title("Prediction of Shallow Net"), plt.legend(["shallow net", "truth"]), plt.show()]
